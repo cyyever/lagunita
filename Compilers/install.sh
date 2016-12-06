f27fe404bc8c4f602d2393bbef178bbd306c98d8
cyyever <cyyever@outlook.com>
2016-12-06 08:23:44 +0800

    add pdf

--- Compilers/install.sh
+++ Compilers/install.sh
@@ -1,8 +1,12 @@
-sudo apt-get install flex bison build-essential csh openjdk-6-jdk libxaw7-dev
-sudo mkdir /usr/class
+sudo apt-get install --no-install-recommends flex bison build-essential csh openjdk-6-jdk libxaw7-dev
+sudo mkdir -p /usr/class
 sudo chown $USER /usr/class
 cd /usr/class
 wget http://spark-university.s3.amazonaws.com/stanford-compilers/vm/student-dist.tar.gz
 tar -xf student-dist.tar.gz
 ln -s /usr/class/cs143/cool ~/cool
-PATH=/usr/class/cs143/cool/bin:$PATH
+new_path='PATH=/usr/class/cs143/cool/bin:$PATH'
+if ! grep "$new_path" ~/.zshrc
+then
+	echo "$new_path" >> ~/.zshrc
+fi
